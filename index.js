fa02c2bbbf1010fb5972e347117f76f01ae331ea
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-23 01:02:14 +0200

    improve perf

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var repeatString = require('repeat-string');
+var repeating = require('repeating');
 
 module.exports = function (str, indent, count) {
 	if (typeof str !== 'string' || typeof indent !== 'string') {
@@ -10,7 +10,7 @@ module.exports = function (str, indent, count) {
 		throw new TypeError('`count` should be a number');
 	}
 
-	indent = count > 1 ? repeatString(indent, count) : indent;
+	indent = count > 1 ? repeating(indent, count) : indent;
 
 	return str.replace(/^(?!\s*$)/mg, indent);
 };
