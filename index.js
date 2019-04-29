859303607575654f1485c2198c9343481e5f420a
Hongarc <anhhong12121997t@gmail.com>
2019-04-29 23:11:32 +0700

    Check `count` >= 0

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ module.exports = (string, count = 1, options) => {
 		);
 	}
 
+	if (count < 0) {
+		throw new RangeError(
+			`Expected \`count\` to be at least 0, got \`${count}\``
+		);
+	}
+
 	if (typeof options.indent !== 'string') {
 		throw new TypeError(
 			`Expected \`options.indent\` to be a \`string\`, got \`${typeof options.indent}\``
